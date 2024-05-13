b2207e0ddce8f19a8f1a0cdcf68de0331e230b92
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-13 19:40:43 +0200

    Čast 1 - hotovo

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,18 +1,30 @@
-import { MenuItem } from '../../components/MenuItem';
-import './style.css';
+import { useState } from "react";
+import { MenuItem } from "../../components/MenuItem";
+import "./style.css";
 
 export const HomePage = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const handleSelectItem = () => {
+    setMenuOpened(!menuOpened);
+  };
+
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={menuOpened ? "menu" : "menu menu--closed"}>
+          <button
+            className="menu__btn"
+            onClick={() => {
+              setMenuOpened(!menuOpened);
+            }}
+          ></button>
           <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
+            <MenuItem text="Domů" onSelect={handleSelectItem} />
+            <MenuItem text="Naše nabídka" onSelect={handleSelectItem} />
+            <MenuItem text="Náš tým" onSelect={handleSelectItem} />
+            <MenuItem text="Blog" onSelect={handleSelectItem} />
+            <MenuItem text="Kontakt" onSelect={handleSelectItem} />
           </div>
         </div>
       </header>
